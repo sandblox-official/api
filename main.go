ff7a7f2780fc4476e7461de28624f4af62aa6a4e
zanefinner <zanefinnergm@gmail.com>
2020-09-04 18:53:04 -0400

    database conn and config

--- main.go
+++ main.go
@@ -5,16 +5,33 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/sandblox-official/api/players"
+
+	"github.com/sandblox-official/api/worlds"
+
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	//Set up and connect database
+	dsn := "zane:5245@tcp(127.0.0.1:3306)/gamedb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Failed to connect database: ", err)
+	}
+	db.AutoMigrate(&worlds.Worlds{})
+	db.AutoMigrate(&players.Players{})
+
+	//Router
 	r := mux.NewRouter()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	err := http.ListenAndServe(":"+port, r)
+	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
