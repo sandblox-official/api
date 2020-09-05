96d39219fa6d8d159ab27fe0b42349d1cb47e4f6
zanefinner <zanefinnergm@gmail.com>
2020-09-05 12:27:01 -0400

    json encoding

--- worlds/blueprints.go
+++ worlds/blueprints.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 //Worlds is the sql blueprint for a stored world
 type Worlds struct {
 	gorm.Model
-	Name  string
-	Owner string
+	Name  string `json:"name"`
+	Owner string `json:"owner"`
 	//InstructionsForBuild will be an odd way to
 	//store world data, but can return direct js
 	//to simplify things for now
-	InstructionsForBuild string
+	InstructionsForBuild string `json:"js"`
 }
